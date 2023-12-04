d295fc948bd18cdd30b5037aa109f232d684ef46
yve-n <yve.nguetche@gmail.com>
2023-12-04 21:27:15 +0100

    remove setting and add restaurant entity and repository

---
+++ src/Entity/Restaurant.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RestaurantRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RestaurantRepository::class)]
+class Restaurant
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $address = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $contact = null;
+
+    #[ORM\OneToOne(mappedBy: 'restaurantId', cascade: ['persist', 'remove'])]
+    private ?Schedule $schedule = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): static
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getContact(): ?string
+    {
+        return $this->contact;
+    }
+
+    public function setContact(string $contact): static
+    {
+        $this->contact = $contact;
+
+        return $this;
+    }
+
+    public function getSchedule(): ?Schedule
+    {
+        return $this->schedule;
+    }
+
+    public function setSchedule(Schedule $schedule): static
+    {
+        // set the owning side of the relation if necessary
+        if ($schedule->getRestaurantId() !== $this) {
+            $schedule->setRestaurantId($this);
+        }
+
+        $this->schedule = $schedule;
+
+        return $this;
+    }
+}
